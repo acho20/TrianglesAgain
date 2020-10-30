6314d08ae4082d45c7645f3402d6032888f2be50
acho20 <acho20@stuy.edu>
2020-10-29 23:03:03 -0400

    fixed rounding error for classify() in Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -33,10 +33,10 @@ public class Triangle{
  }
 
  public String classify(){
-  double d1 = (Math.round(10000 * v1.distanceTo(v2)))/10000;
-  double d2 = (Math.round(10000 * v1.distanceTo(v3)))/10000;
-  double d3 = (Math.round(10000 * v2.distanceTo(v3)))/10000;
-  if (d1 == d2 && d2 == d3){
+  double d1 = ((double)Math.round(10000 * v1.distanceTo(v2)))/10000;
+  double d2 = ((double)Math.round(10000 * v1.distanceTo(v3)))/10000;
+  double d3 = ((double)Math.round(10000 * v2.distanceTo(v3)))/10000;
+  if (d1 == d2 && d2 == d3 && d1 == d3){
    return "equilateral";
   }else if (d1 == d2 || d1 == d3 || d2 == d3){
    return "isosceles";
